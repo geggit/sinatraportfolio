c335e355a096cccc9780b0b5b58cf2c09cfbca5f
geggit <ronbeli@hotmail.com>
2012-07-09 01:58:05 +0200

    Added Sinatra portfolio with different color link

--- sinatraportfolio.rb
+++ sinatraportfolio.rb
@@ -17,6 +17,7 @@ __END__
       h3 {color:yellow; margin:auto; width:50%; background-color:#b0e0e6; text-align:center}
       h4 {color:green; margin:auto; width:80%; background-color:#b0e0e6;}
       h2 {color:red; margin:auto; width:30%; background-color:#b0e0e6; text-align:center; transform: rotate(30deg); -ms-transform: rotate(30deg); /* IE 9 */ -webkit-transform: rotate(30deg); /* Safari and Chrome */ -o-transform: rotate(30deg); /* Opera */ -moz-transform: rotate(30deg); /* Firefox */;}
+      a {color:red}
   %body
     #container
       %header
