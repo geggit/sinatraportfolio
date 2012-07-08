3296f767d8a45aa7f8e1e49d24d369c38e1f5544
geggit <ronbeli@hotmail.com>
2012-07-08 20:27:07 +0200

    Vth commit added haml link

--- sinatraportfolio.rb
+++ sinatraportfolio.rb
@@ -11,6 +11,9 @@ __END__
 @@index
 %html
   %body
-    %h2 "This is my Sinatra Portfolio (pretty empty for now) and below is the link to my RAILS PORTFOLIO:"
-    %h3 = "http://railsportfolio.heroku.com"
+    %h2 "This is my Sinatra Portfolio 
+    %h3 "(pretty empty for now)" q
+    %h3 "and below is the link to my RAILS PORTFOLIO:"
+    %h3 
+      %a ="http://railsportfolio.heroku.com"
 
