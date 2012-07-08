3c0bf12834d531dc87d9f14e8777242f10ad0535
geggit <ronbeli@hotmail.com>
2012-07-08 20:30:21 +0200

    6th commit added haml gem to SinatraPortfolio

--- sinatraportfolio.rb
+++ sinatraportfolio.rb
@@ -11,9 +11,9 @@ __END__
 @@index
 %html
   %body
-    %h2 "This is my Sinatra Portfolio 
-    %h3 "(pretty empty for now)" q
-    %h3 "and below is the link to my RAILS PORTFOLIO:"
+    %h2 This is my Sinatra Portfolio 
+    %h3 (pretty empty for now)
+    %h3 and below is the link to my RAILS PORTFOLIO:
     %h3 
-      %a ="http://railsportfolio.heroku.com"
+      %a http://railsportfolio.heroku.com
 
