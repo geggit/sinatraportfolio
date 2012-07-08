100db3857d247e933a579b0bbc884836811373f7
geggit <ronbeli@hotmail.com>
2012-07-08 20:12:51 +0200

    Second commit after changes to SinatraPortfolio

--- sinatraportfolio.rb
+++ sinatraportfolio.rb
@@ -1,5 +1,17 @@
-require 'sinatra'
+require 'sinatra'l
+require 'haml'
+
 
 get '/' do
   "My Sinatra Portfolio"
+  haml :index
 end
+
+__END__
+
+@@index
+%html
+  %body
+    %h2 "This is my Sinatra Portfolio (pretty empty for now) and below is the link to my RAILS PORTFOLIO:"
+    %h3 = "http://railsportfolio.heroku.com"
+
