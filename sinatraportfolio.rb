4d96572284e22de3e07586842906975879a07edb
geggit <ronbeli@hotmail.com>
2012-07-11 11:24:39 +0200

    Changed to google's web font-family Tangerine

--- sinatraportfolio.rb
+++ sinatraportfolio.rb
@@ -12,8 +12,9 @@ __END__
 %html
   %head
     %style 
+    %link{:href => "http://fonts.googleapis.com/css?family=Tangerine", :rel => "stylesheet", :type => "text/css"}  
     %style{:type => "text/css"}
-      h2 {font-family:calibri; color:blue; margin:auto; width:90%; background-color:#b0e0e6; text-shadow: 5px 5px 5px yellow; text-align:center}
+      h2 {font-family: 'Tangerine', serif; font-size: 50px; color:blue; margin:auto; width:90%; background-color:#b0e0e6; text-shadow: 5px 5px 5px yellow; text-align:center}
       h5 {font-family:calibri; color:yellow; margin:auto; width:50%; background-color:#b0e0e6; text-align:center}
       h4 {color:green; margin:auto; width:50%; background-color:#b0e0e6;}
       h3 {color:white; margin:auto; position:fixed; top:300px; left:0px; width:30%; background-color:green; text-align:center; transform: rotate(-100deg); -ms-transform: rotate(-100deg); /* IE 9 */ -webkit-transform: rotate(-100deg); /* Safari and Chrome */ -o-transform: rotate(-100deg); /* Opera */ -moz-transform: rotate(-100deg); /* Firefox */;}
