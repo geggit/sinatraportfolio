375c1a5b257352f5ea77a2ae095bbf3163e59764
geggit <ronbeli@hotmail.com>
2012-07-11 10:00:51 +0200

    Changed font family to calibri

--- sinatraportfolio.rb
+++ sinatraportfolio.rb
@@ -13,17 +13,17 @@ __END__
   %head
     %style 
     %style{:type => "text/css"}
-      h1 {color:blue; margin:auto; width:90%; background-color:#b0e0e6; text-shadow: 5px 5px 5px yellow; text-align:center}
-      h5 {color:yellow; margin:auto; width:50%; background-color:#b0e0e6; text-align:center}
+      h2 {font-family:calibri; color:blue; margin:auto; width:90%; background-color:#b0e0e6; text-shadow: 5px 5px 5px yellow; text-align:center}
+      h5 {font-family:calibri; color:yellow; margin:auto; width:50%; background-color:#b0e0e6; text-align:center}
       h4 {color:green; margin:auto; width:50%; background-color:#b0e0e6;}
-      h2 {color:white; margin:auto; position:fixed; top:300px; left:0px; width:30%; background-color:green; text-align:center; transform: rotate(-100deg); -ms-transform: rotate(-100deg); /* IE 9 */ -webkit-transform: rotate(-100deg); /* Safari and Chrome */ -o-transform: rotate(-100deg); /* Opera */ -moz-transform: rotate(-100deg); /* Firefox */;}
-      h2 a {color:white}
-      a {color:blue}
+      h3 {color:white; margin:auto; position:fixed; top:300px; left:0px; width:30%; background-color:green; text-align:center; transform: rotate(-100deg); -ms-transform: rotate(-100deg); /* IE 9 */ -webkit-transform: rotate(-100deg); /* Safari and Chrome */ -o-transform: rotate(-100deg); /* Opera */ -moz-transform: rotate(-100deg); /* Firefox */;}
+      h3 a {color:white}
+      a {font-family:calibri; color:blue}
   %body
     #container
       %header
         %br  
-        %h1 This is my Sinatra Portfolio 
+        %h2 This is my Sinatra Portfolio 
         %br
         %h5 (pretty empty for now at initial stage, but soon it will grow...)
       #main
@@ -40,6 +40,6 @@ __END__
           %a{} 2. (to be continued soon)
         %br
       #link 
-        %h2 
+        %h3 
           %a{:href=>"http://railsportfolio.heroku.com"} RAILS portfolio
 
